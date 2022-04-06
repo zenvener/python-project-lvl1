c63cec9da08c2182a0de8dfd20f3284626469021
zenvener <zenxiontrue@gmail.com>
2022-04-06 21:40:55 +0500

    putting a new prompt function

---
+++ brain_games/cli.py
@@ -0,0 +1,6 @@
+def welcome_user():
+
+    import prompt
+    name = prompt.string("May I have your name? ")
+    print("Hello, {}!". format(name))
+
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,6 +1,16 @@
+from brain_games.cli import welcome_user
+
+
 def main():
    print("Welcome to the Brain Games!")
 
 
+def start():
+    main()
+
+
 if __name__ == "__main__":
     main()
+
+
+welcome_user()
