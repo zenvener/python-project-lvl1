2fa2eb27b010a8ace72cb46dce30b6bcd34b27a6
zenvener <zenxiontrue@gmail.com>
2022-05-06 23:16:34 +0500

    adding new changes into brain_prime

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
+from brain_games.engine import launch
 from brain_games.games import prime
 
 
 def main():
-    prime.start_prime()
+    launch(prime)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
