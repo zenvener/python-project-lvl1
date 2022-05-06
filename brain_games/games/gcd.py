b9fa810deac792aa93c5bffb7e641d5ce42d97b1
zenvener <zenxiontrue@gmail.com>
2022-05-07 00:03:57 +0500

    modifying the gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -3,6 +3,7 @@ from random import randint
 
 game_set = "Find the greatest common divisor of given numbers."
 
+
 def find_gcd(q, w):
     if q < w:
         q, w = w, q
@@ -12,6 +13,8 @@ def find_gcd(q, w):
         w = div_remainder
         div_remainder = q % w
     return w
+
+
 def get_question_and_decision():
     start_num = 1
     end_num = 20
