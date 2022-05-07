d77c42f9c459c65526361a29456d0a9cc8a947ae
zenvener <zenxiontrue@gmail.com>
2022-05-07 21:31:18 +0500

    modifying the progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -2,36 +2,25 @@
 from random import randint
 
 
-game_set = "What number is missing in the progression?"
-START_NUMBER = 1
-OVERALL_NUMBERS = 100
-MIN_HID_NUMBER = 1
-MAX_HID_NUMBER = 9
-MIN_NUM_PROGRESSION = 1
-MAX_NUM_PROGRESSION = 10
+game_set = 'What number is missing in the progression?'
 
 
+def get_question_and_decision():
+    first_number = randint(0, 900)
+    random_index = randint(1, range_length - 1)
+    scale_step = randint(1, 5)
+    overall_length = randint(5, 10)
+    
+    
 
-def constr_progression(core_element, step):
-    member, progression = core_element, [core_element]
-    for i in range(10):
-        member += step
-        progression.append(member)
-    return progression
+    progression = list(range(
+        first_number, first_number + overall_length * scale_step, scale_step))
+    progression = [str(i) for i in progression]
 
+    correct_answer = progression[random_index]
 
-def get_sentence_out_of_progression(progression, hid_number):
-    progression_string = []
-    for index in range(0, 10):
-        progression_string.append(str(progression[index]))
-    progression_string[hid_number] = '..'
-    return " ".join(progression_string)
+    progression[random_index] = '..'
 
+    question = " ".join(progression)
 
-def get_play_overall():
-    first_element = randint(START_NUMBER, OVERALL_NUMBERS)
-    step = randint(MIN_NUM_PROGRESSION, MAX_NUM_PROGRESSION)
-    hidden_number = randint(MIN_HID_NUMBER, MAX_HID_NUMBER)
-    progression = construct_progression(first_element, step)
-    return (get_sentence_out_of_progression(progression, hidden_number),
-            str(progression[hidden_number]))
+    return (question, correct_answer)
