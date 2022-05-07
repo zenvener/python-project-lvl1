0f18ede446b697fa64437bc1a408f5c4407abd3d
zenvener <zenxiontrue@gmail.com>
2022-05-07 21:47:26 +0500

    changing code of the progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -4,14 +4,14 @@ from random import randint
 
 game_set = 'What number is missing in the progression?'
 
+    
+
+
+def get_question_and_decision():
     START_NUMBER = randint(0, 900)
     RANGE_LENGTH = randint(5, 10)
     STEP_RANGE = randint(1, 5)
     RANDOM_INDEX = randint(1, RANGE_LENGTH - 1)
-
-
-def get_question_and_decision():
-    
 
     progression = list(range(
         START_NUMBER, START_NUMBER + RANGE_LENGTH * STEP_RANGE, STEP_RANGE))
