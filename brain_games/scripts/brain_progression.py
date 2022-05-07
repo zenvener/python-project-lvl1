8541ee53b93074af81675f090482f325175dd6d5
zenvener <zenxiontrue@gmail.com>
2022-05-07 21:34:28 +0500

    adding new changes to the brain_progression.py

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,10 +1,12 @@
-#!/usr/bin/env python3
+#! /usr/bin/env python3
+
+from brain_games.engine import launch
 from brain_games.games import progression
 
 
 def main():
-    progression.start_progression()
+    launch(progression)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
