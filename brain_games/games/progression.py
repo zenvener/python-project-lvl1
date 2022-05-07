3a8221b05ec02b47e112945f13a5365869c90b49
zenvener <zenxiontrue@gmail.com>
2022-05-07 21:21:08 +0500

    changing the code of the progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,26 +1,37 @@
-import random
-import prompt
+#!/usr/bin/env python3
+from random import randint
 
 
-def start_progression():
-    print("Welcome to the Brain Games!")
-    name = prompt.string("May I have your name? ")
-    print(f"Hello, {name}!")
-    print('What number is missing in the progression?')
-    for i in range(3):
-        a, d = random.randint(1, 100), random.randint(1, 10)
-        p = [int(i) for i in range(a, a + d * 9 + 1, d)]
-        r_ans = random.choice(p)
-        r_index = p.index(r_ans)
-        p[r_index] = ".."
-        print("Question: ", end="")
-        print(*p)
-        ans = int(input("Your answer: "))
-        if r_ans == ans:
-            print("Correct!")
-        else:
-            print(f"'{ans}' is wrong answer ;(. Correct answer was '{r_ans}'.")
-            print(f"Let's try again, {name}!")
-            break
-    else:
-        print(f"Congratulations, {name}!")
+game_set = "What number is missing in the progression?"
+START_NUMBER = 1
+OVERALL_NUMBERS = 100
+MIN_HID_NUMBER = 1
+MAX_HID_NUMBER = 9
+MIN_NUM_PROGRESSION = 1
+MAX_NUM_PROGRESSION = 10
+
+
+
+def constr_progression(core_element, step):
+    member, progression = core_element, [core_element]
+    for i in range(10):
+        member += step
+        progression.append(member)
+    return progression
+
+
+def get_sentence_out_of_progression(progression, hid_number):
+    progression_string = []
+    for index in range(0, 10):
+        progression_string.append(str(progression[index]))
+    progression_string[hid_number] = '..'
+    return " ".join(progression_string)
+
+
+def get_play_overall():
+    first_element = randint(START_NUMBER, OVERALL_NUMBERS)
+    step = randint(MIN_NUM_PROGRESSION, MAX_NUM_PROGRESSION)
+    hidden_number = randint(MIN_HID_NUMBER, MAX_HID_NUMBER)
+    progression = construct_progression(first_element, step)
+    return (get_sentence_out_of_progression(progression, hidden_number),
+            str(progression[hidden_number]))
