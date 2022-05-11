317180299abe88474a88334cfb94dfca35332f2e
zenvener <zenxiontrue@gmail.com>
2022-05-11 21:48:40 +0500

    changing the constants of the games from lower_cases to upper_cases

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -8,7 +8,7 @@ def launch(game):
     print(game.game_set)
     MAXIMUM_ROUNDS = 3
     for _ in range(MAXIMUM_ROUNDS):
-        question, correct_answer = game.get_question_and_decision()
+        question, correct_answer = game.get_question_and_answer()
         print(f'Question: {question}')
         user_answer = input('Your answer: ')
         if user_answer != correct_answer:
